f343f725aa4beeaa9d22782b1ed418f105bf33bb
Yuga Bharathi Jaisankar <yuga.bharathijai2106@gmail.com>
2026-06-29 13:49:29 +0530

    feat: add ox_lib dependency and shared script initialization to fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,8 +4,12 @@ game 'gta5'
 description 'SPiceZ Nitrous System'
 version '1.0.5'
 
+shared_script '@ox_lib/init.lua'
+
 client_script 'client/main.lua'
 
 server_scripts {
     'server/main.lua'
 }
+
+dependency 'ox_lib'
